ac404a554ed8fecc20dccdb57c0a29ed424f753a
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-06-30 15:24:32 +0800

    Update siteseal.js

--- lib4htmlpage/js/siteseal.js
+++ lib4htmlpage/js/siteseal.js
@@ -2,8 +2,11 @@ var connect_protocol=window.location.protocol;
 //Only Give trust on https
 var connect_domain=window.location.hostname;
 //Get Host Domain
+var myHeading = document.querySelector("h1");
+
 if (connect_protocol==="https:"){
     alert("Connect through https"+window.location.pathname);
+    myHeading.textContent = "Connect through https"+window.location.pathname;
 }else{
     alert("Connect through http")
 }
